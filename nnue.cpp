6c0c4dd1ea0c511d2373c2391edcb35778dd5162
alve <alve.lindell@gmail.com>
2026-03-15 14:17:12 +0100

    non avx compatability

--- nnue.cpp
+++ nnue.cpp
@@ -7,20 +7,17 @@
 #define ACTIVATION_CLIP 256
 #define FEATURE_QUANT 64
 
-
-int calculate_idx(int piece_type, int side, int square, int perspective)
-{
+int calculate_idx(int piece_type, int side, int square, int perspective) {
     perspective = perspective ^ 1;
     side = side ^ 1;
     if (perspective == 1) {
-        side = side ^ 1;          
+        side = side ^ 1;
         square = square ^ 0b111000;
     }
     return side * 64 * 6 + piece_type * 64 + square;
 }
 
-
-void nnue_init(AccumulatorPair* pair, const Board &brd) {
+void nnue_init(AccumulatorPair *pair, const Board &brd) {
     for (int i = 0; i < HL_SIZE; i++) {
         pair->white.values[i] = FEATURE_BIAS[i];
         pair->black.values[i] = FEATURE_BIAS[i];
@@ -32,18 +29,43 @@ void nnue_init(AccumulatorPair* pair, const Board &brd) {
         int square = __builtin_ctzll(occ);
         int piece_type, piece_color;
 
-        if ((1ULL << square) & brd.WPawn) { piece_type = 0; piece_color = 1; }
-        else if ((1ULL << square) & brd.WKnight) { piece_type = 1; piece_color = 1; }
-        else if ((1ULL << square) & brd.WBishop) { piece_type = 2; piece_color = 1; }
-        else if ((1ULL << square) & brd.WRook) { piece_type = 3; piece_color = 1; }
-        else if ((1ULL << square) & brd.WQueen) { piece_type = 4; piece_color = 1; }
-        else if ((1ULL << square) & brd.WKing) { piece_type = 5; piece_color = 1; }
-        else if ((1ULL << square) & brd.BPawn) { piece_type = 0; piece_color = 0; }
-        else if ((1ULL << square) & brd.BKnight) { piece_type = 1; piece_color = 0; }
-        else if ((1ULL << square) & brd.BBishop) { piece_type = 2; piece_color = 0; }
-        else if ((1ULL << square) & brd.BRook) { piece_type = 3; piece_color = 0; }
-        else if ((1ULL << square) & brd.BQueen) { piece_type = 4; piece_color = 0; }
-        else if ((1ULL << square) & brd.BKing) { piece_type = 5; piece_color = 0; }
+        if ((1ULL << square) & brd.WPawn) {
+            piece_type = 0;
+            piece_color = 1;
+        } else if ((1ULL << square) & brd.WKnight) {
+            piece_type = 1;
+            piece_color = 1;
+        } else if ((1ULL << square) & brd.WBishop) {
+            piece_type = 2;
+            piece_color = 1;
+        } else if ((1ULL << square) & brd.WRook) {
+            piece_type = 3;
+            piece_color = 1;
+        } else if ((1ULL << square) & brd.WQueen) {
+            piece_type = 4;
+            piece_color = 1;
+        } else if ((1ULL << square) & brd.WKing) {
+            piece_type = 5;
+            piece_color = 1;
+        } else if ((1ULL << square) & brd.BPawn) {
+            piece_type = 0;
+            piece_color = 0;
+        } else if ((1ULL << square) & brd.BKnight) {
+            piece_type = 1;
+            piece_color = 0;
+        } else if ((1ULL << square) & brd.BBishop) {
+            piece_type = 2;
+            piece_color = 0;
+        } else if ((1ULL << square) & brd.BRook) {
+            piece_type = 3;
+            piece_color = 0;
+        } else if ((1ULL << square) & brd.BQueen) {
+            piece_type = 4;
+            piece_color = 0;
+        } else if ((1ULL << square) & brd.BKing) {
+            piece_type = 5;
+            piece_color = 0;
+        }
 
         int white_idx = calculate_idx(piece_type, piece_color, square, 1);
         int black_idx = calculate_idx(piece_type, piece_color, square, 0);
@@ -53,13 +75,12 @@ void nnue_init(AccumulatorPair* pair, const Board &brd) {
             pair->black.values[i] += FEATURE_WEIGHTS[black_idx][i];
         }
 
-        occ &= occ - 1; 
+        occ &= occ - 1;
     }
 }
 
-
-
-void accumulatorAddPiece(AccumulatorPair* pair, int piece_type, int piece_color, int square) {
+void accumulatorAddPiece(AccumulatorPair *pair, int piece_type, int piece_color,
+                         int square) {
     int white_idx = calculate_idx(piece_type, piece_color, square, 1);
     int black_idx = calculate_idx(piece_type, piece_color, square, 0);
 
@@ -69,7 +90,8 @@ void accumulatorAddPiece(AccumulatorPair* pair, int piece_type, int piece_color,
     }
 }
 
-void accumulatorSubPiece(AccumulatorPair* pair, int piece_type, int piece_color, int square) {
+void accumulatorSubPiece(AccumulatorPair *pair, int piece_type, int piece_color,
+                         int square) {
     int white_idx = calculate_idx(piece_type, piece_color, square, 1);
     int black_idx = calculate_idx(piece_type, piece_color, square, 0);
 
@@ -79,18 +101,23 @@ void accumulatorSubPiece(AccumulatorPair* pair, int piece_type, int piece_color,
     }
 }
 
-void accumulatorSubAddPiece(AccumulatorPair* pair, int piece_type, int piece_color, int from, int to) {
+void accumulatorSubAddPiece(AccumulatorPair *pair, int piece_type,
+                            int piece_color, int from, int to) {
     int wi_rem = calculate_idx(piece_type, piece_color, from, 1);
     int bi_rem = calculate_idx(piece_type, piece_color, from, 0);
     int wi_add = calculate_idx(piece_type, piece_color, to, 1);
     int bi_add = calculate_idx(piece_type, piece_color, to, 0);
 
     for (int i = 0; i < HL_SIZE; i++) {
-        pair->white.values[i] += FEATURE_WEIGHTS[wi_add][i] - FEATURE_WEIGHTS[wi_rem][i];
-        pair->black.values[i] += FEATURE_WEIGHTS[bi_add][i] - FEATURE_WEIGHTS[bi_rem][i];
+        pair->white.values[i] +=
+            FEATURE_WEIGHTS[wi_add][i] - FEATURE_WEIGHTS[wi_rem][i];
+        pair->black.values[i] +=
+            FEATURE_WEIGHTS[bi_add][i] - FEATURE_WEIGHTS[bi_rem][i];
     }
 }
-void accumulatorSubAddCapture(AccumulatorPair* pair, int piece_type, int piece_color, int cap_type, int cap_color, int from, int to, bool undo) {
+void accumulatorSubAddCapture(AccumulatorPair *pair, int piece_type,
+                              int piece_color, int cap_type, int cap_color,
+                              int from, int to, bool undo) {
     int wi_rem = calculate_idx(piece_type, piece_color, from, 1);
     int bi_rem = calculate_idx(piece_type, piece_color, from, 0);
     int wi_add = calculate_idx(piece_type, piece_color, to, 1);
@@ -100,20 +127,21 @@ void accumulatorSubAddCapture(AccumulatorPair* pair, int piece_type, int piece_c
     int cap_sign = undo ? 1 : -1;
 
     for (int i = 0; i < HL_SIZE; i++) {
-        pair->white.values[i] += FEATURE_WEIGHTS[wi_add][i] 
-                                - FEATURE_WEIGHTS[wi_rem][i] 
-                                + cap_sign * FEATURE_WEIGHTS[wi_cap][i];
-        pair->black.values[i] += FEATURE_WEIGHTS[bi_add][i] 
-                                - FEATURE_WEIGHTS[bi_rem][i] 
-                                + cap_sign * FEATURE_WEIGHTS[bi_cap][i];
+        pair->white.values[i] += FEATURE_WEIGHTS[wi_add][i] -
+                                 FEATURE_WEIGHTS[wi_rem][i] +
+                                 cap_sign * FEATURE_WEIGHTS[wi_cap][i];
+        pair->black.values[i] += FEATURE_WEIGHTS[bi_add][i] -
+                                 FEATURE_WEIGHTS[bi_rem][i] +
+                                 cap_sign * FEATURE_WEIGHTS[bi_cap][i];
     }
 }
 
 /*
 
-void accumulatorAddPiece(AccumulatorPair* pair, int piece_type, int piece_color, int square) {
-    const int white_idx = calculate_idx(piece_type, piece_color, square, 1);
-    const int black_idx = calculate_idx(piece_type, piece_color, square, 0);
+void accumulatorAddPiece(AccumulatorPair* pair, int piece_type, int piece_color,
+int square) { const int white_idx = calculate_idx(piece_type, piece_color,
+square, 1); const int black_idx = calculate_idx(piece_type, piece_color, square,
+0);
 
     const __m512i* w_ptr = (__m512i*)&FEATURE_WEIGHTS[white_idx][0];
     const __m512i* b_ptr = (__m512i*)&FEATURE_WEIGHTS[black_idx][0];
@@ -132,9 +160,10 @@ void accumulatorAddPiece(AccumulatorPair* pair, int piece_type, int piece_color,
     }
 }
 
-void accumulatorSubPiece(AccumulatorPair* pair, int piece_type, int piece_color, int square) {
-    const int white_idx = calculate_idx(piece_type, piece_color, square, 1);
-    const int black_idx = calculate_idx(piece_type, piece_color, square, 0);
+void accumulatorSubPiece(AccumulatorPair* pair, int piece_type, int piece_color,
+int square) { const int white_idx = calculate_idx(piece_type, piece_color,
+square, 1); const int black_idx = calculate_idx(piece_type, piece_color, square,
+0);
 
     const __m512i* w_ptr = (__m512i*)&FEATURE_WEIGHTS[white_idx][0];
     const __m512i* b_ptr = (__m512i*)&FEATURE_WEIGHTS[black_idx][0];
@@ -157,50 +186,120 @@ void accumulatorSubPiece(AccumulatorPair* pair, int piece_type, int piece_color,
 */
 
 #include <cstring>
+int nnue_evaluate(AccumulatorPair *pair, int side_to_move) {
+    const int16_t *own =
+        (side_to_move == 0) ? pair->white.values : pair->black.values;
+    const int16_t *opp =
+        (side_to_move == 0) ? pair->black.values : pair->white.values;
 
-int nnue_evaluate(AccumulatorPair* pair, int side_to_move) {
-    const int16_t* own = (side_to_move == 0) ? pair->white.values : pair->black.values;
-    const int16_t* opp = (side_to_move == 0) ? pair->black.values : pair->white.values;
-
+#if defined(__AVX512F__) && defined(__AVX512BW__)
     const __m512i zero = _mm512_setzero_si512();
     const __m512i clip = _mm512_set1_epi16(ACTIVATION_CLIP);
-
     __m512i acc = _mm512_setzero_si512();
 
     for (int i = 0; i < HL_SIZE; i += 32) {
-        __m512i own_v = _mm512_loadu_si512((__m512i*)&own[i]);
-        __m512i opp_v = _mm512_loadu_si512((__m512i*)&opp[i]);
-
+        __m512i own_v = _mm512_loadu_si512((__m512i *)&own[i]);
+        __m512i opp_v = _mm512_loadu_si512((__m512i *)&opp[i]);
         own_v = _mm512_min_epi16(_mm512_max_epi16(own_v, zero), clip);
         opp_v = _mm512_min_epi16(_mm512_max_epi16(opp_v, zero), clip);
-
-        __m512i w_own = _mm512_load_si512((__m512i*)&OUTPUT_WEIGHTS[i]);
-        __m512i w_opp = _mm512_load_si512((__m512i*)&OUTPUT_WEIGHTS[HL_SIZE + i]);
-
+        __m512i w_own = _mm512_load_si512((__m512i *)&OUTPUT_WEIGHTS[i]);
+        __m512i w_opp =
+            _mm512_load_si512((__m512i *)&OUTPUT_WEIGHTS[HL_SIZE + i]);
         __m512i own_lo = _mm512_cvtepi16_epi32(_mm512_castsi512_si256(own_v));
-        __m512i own_hi = _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(own_v, 1));
+        __m512i own_hi =
+            _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(own_v, 1));
         __m512i opp_lo = _mm512_cvtepi16_epi32(_mm512_castsi512_si256(opp_v));
-        __m512i opp_hi = _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(opp_v, 1));
-
+        __m512i opp_hi =
+            _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(opp_v, 1));
         __m512i wown_lo = _mm512_cvtepi16_epi32(_mm512_castsi512_si256(w_own));
-        __m512i wown_hi = _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(w_own, 1));
+        __m512i wown_hi =
+            _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(w_own, 1));
         __m512i wopp_lo = _mm512_cvtepi16_epi32(_mm512_castsi512_si256(w_opp));
-        __m512i wopp_hi = _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(w_opp, 1));
-
-        acc = _mm512_add_epi32(acc, _mm512_mullo_epi32(_mm512_mullo_epi32(own_lo, own_lo), wown_lo));
-        acc = _mm512_add_epi32(acc, _mm512_mullo_epi32(_mm512_mullo_epi32(own_hi, own_hi), wown_hi));
-        acc = _mm512_add_epi32(acc, _mm512_mullo_epi32(_mm512_mullo_epi32(opp_lo, opp_lo), wopp_lo));
-        acc = _mm512_add_epi32(acc, _mm512_mullo_epi32(_mm512_mullo_epi32(opp_hi, opp_hi), wopp_hi));
+        __m512i wopp_hi =
+            _mm512_cvtepi16_epi32(_mm512_extracti64x4_epi64(w_opp, 1));
+        acc = _mm512_add_epi32(
+            acc,
+            _mm512_mullo_epi32(_mm512_mullo_epi32(own_lo, own_lo), wown_lo));
+        acc = _mm512_add_epi32(
+            acc,
+            _mm512_mullo_epi32(_mm512_mullo_epi32(own_hi, own_hi), wown_hi));
+        acc = _mm512_add_epi32(
+            acc,
+            _mm512_mullo_epi32(_mm512_mullo_epi32(opp_lo, opp_lo), wopp_lo));
+        acc = _mm512_add_epi32(
+            acc,
+            _mm512_mullo_epi32(_mm512_mullo_epi32(opp_hi, opp_hi), wopp_hi));
     }
-
-    int32_t temp[16];
-    _mm512_store_si512((__m512i*)temp, acc);
 
     __m256i lo = _mm512_castsi512_si256(acc);
     __m256i hi = _mm512_extracti64x4_epi64(acc, 1);
     __m256i sum = _mm256_add_epi32(lo, hi);
     sum = _mm256_hadd_epi32(sum, sum);
     sum = _mm256_hadd_epi32(sum, sum);
-    int32_t output = OUTPUT_BIAS + _mm256_extract_epi32(sum, 0) + _mm256_extract_epi32(sum, 4);
-    return output / (256*64);
+    int32_t output = OUTPUT_BIAS + _mm256_extract_epi32(sum, 0) +
+                     _mm256_extract_epi32(sum, 4);
+
+#elif defined(__AVX2__)
+    const __m256i zero = _mm256_setzero_si256();
+    const __m256i clip = _mm256_set1_epi16(ACTIVATION_CLIP);
+    __m256i acc = _mm256_setzero_si256();
+
+    for (int i = 0; i < HL_SIZE; i += 16) {
+        __m256i own_v = _mm256_loadu_si256((__m256i *)&own[i]);
+        __m256i opp_v = _mm256_loadu_si256((__m256i *)&opp[i]);
+        own_v = _mm256_min_epi16(_mm256_max_epi16(own_v, zero), clip);
+        opp_v = _mm256_min_epi16(_mm256_max_epi16(opp_v, zero), clip);
+        __m256i w_own = _mm256_load_si256((__m256i *)&OUTPUT_WEIGHTS[i]);
+        __m256i w_opp =
+            _mm256_load_si256((__m256i *)&OUTPUT_WEIGHTS[HL_SIZE + i]);
+        __m256i own_lo = _mm256_cvtepi16_epi32(_mm256_castsi256_si128(own_v));
+        __m256i own_hi =
+            _mm256_cvtepi16_epi32(_mm256_extracti128_si256(own_v, 1));
+        __m256i opp_lo = _mm256_cvtepi16_epi32(_mm256_castsi256_si128(opp_v));
+        __m256i opp_hi =
+            _mm256_cvtepi16_epi32(_mm256_extracti128_si256(opp_v, 1));
+        __m256i wown_lo = _mm256_cvtepi16_epi32(_mm256_castsi256_si128(w_own));
+        __m256i wown_hi =
+            _mm256_cvtepi16_epi32(_mm256_extracti128_si256(w_own, 1));
+        __m256i wopp_lo = _mm256_cvtepi16_epi32(_mm256_castsi256_si128(w_opp));
+        __m256i wopp_hi =
+            _mm256_cvtepi16_epi32(_mm256_extracti128_si256(w_opp, 1));
+        acc = _mm256_add_epi32(
+            acc,
+            _mm256_mullo_epi32(_mm256_mullo_epi32(own_lo, own_lo), wown_lo));
+        acc = _mm256_add_epi32(
+            acc,
+            _mm256_mullo_epi32(_mm256_mullo_epi32(own_hi, own_hi), wown_hi));
+        acc = _mm256_add_epi32(
+            acc,
+            _mm256_mullo_epi32(_mm256_mullo_epi32(opp_lo, opp_lo), wopp_lo));
+        acc = _mm256_add_epi32(
+            acc,
+            _mm256_mullo_epi32(_mm256_mullo_epi32(opp_hi, opp_hi), wopp_hi));
+    }
+
+    __m128i lo = _mm256_castsi256_si128(acc);
+    __m128i hi = _mm256_extracti128_si256(acc, 1);
+    __m128i sum = _mm_add_epi32(lo, hi);
+    sum = _mm_hadd_epi32(sum, sum);
+    sum = _mm_hadd_epi32(sum, sum);
+    int32_t output = OUTPUT_BIAS + _mm_cvtsi128_si32(sum);
+
+#else
+    /* Scalar fallback — no SIMD required */
+    int32_t acc = 0;
+    for (int i = 0; i < HL_SIZE; i++) {
+        int32_t o = own[i] < 0                 ? 0
+                    : own[i] > ACTIVATION_CLIP ? ACTIVATION_CLIP
+                                               : own[i];
+        int32_t p = opp[i] < 0                 ? 0
+                    : opp[i] > ACTIVATION_CLIP ? ACTIVATION_CLIP
+                                               : opp[i];
+        acc += (int32_t)o * o * OUTPUT_WEIGHTS[i];
+        acc += (int32_t)p * p * OUTPUT_WEIGHTS[HL_SIZE + i];
+    }
+    int32_t output = OUTPUT_BIAS + acc;
+#endif
+
+    return output / (256 * 64);
 }
